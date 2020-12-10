8ca00f011616844bc33c754d237d8d0ff53a79de
nhp2907 <40029330+nhp2907@users.noreply.github.com>
2020-12-11 00:42:48 +0700

    Update course-detail

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Category = require('../models/category')
 const user = {};
 const AuthService = require('../service/auth.service')
+const Course = require("../models/course");
 
 
 router.get('/auth', (req, res) => {
@@ -51,36 +52,36 @@ router.get('/', (req, res) => {
                 author: 'Jose Portilla',
                 price: '11.99',
                 pre_price: '129.99'
-                },
-            {
-                img: 'https://courseupload.com/wp-content/uploads/2020/10/Screen-Shot-2020-10-27-at-2.20.51-PM.png',
-                name: 'WordPress Become a WordPress Developer: Unlocking Power With Code',
-                author: 'Brad Schiff',
-                price: '16.79',
-                pre_price: '179.99'
             },
-            {
-                img: 'https://freecourseudemy.com/wp-content/uploads/2020/01/The-Modern-React-Bootcamp-Hooks-Context-NextJS-Router.jpg',
-                name: 'The Modern React Bootcamp (Hooks, Context, NextJS, Router)',
-                author: 'Colt Steele',
-                price: '11.99',
-                pre_price: '129.99'
-            }],
+                {
+                    img: 'https://courseupload.com/wp-content/uploads/2020/10/Screen-Shot-2020-10-27-at-2.20.51-PM.png',
+                    name: 'WordPress Become a WordPress Developer: Unlocking Power With Code',
+                    author: 'Brad Schiff',
+                    price: '16.79',
+                    pre_price: '179.99'
+                },
+                {
+                    img: 'https://freecourseudemy.com/wp-content/uploads/2020/01/The-Modern-React-Bootcamp-Hooks-Context-NextJS-Router.jpg',
+                    name: 'The Modern React Bootcamp (Hooks, Context, NextJS, Router)',
+                    author: 'Colt Steele',
+                    price: '11.99',
+                    pre_price: '129.99'
+                }],
             course: [{
                 img: 'https://i.ytimg.com/vi/Stj9I9_Q_6w/maxresdefault.jpg',
                 name: 'Angular 2 & 4 complete tutorial with additional firebase Your progress',
                 progress: '0'
             },
-            {
-                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDAhkyqgta5EHx9SYy3VqReC5TLek-cSDWAA&usqp=CAU',
-                name: 'MEAN Stack For Web Developers: Build Websites on Javascript',
-                progress: '25'
-            },
-            {
-                img: 'https://desirecourse.net/wp-content/uploads/2018/09/7867856.jpg',
-                name: 'The Coding Interview Bootcamp: Algorithms + Data Structures',
-                progress: '80'
-            }],
+                {
+                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDAhkyqgta5EHx9SYy3VqReC5TLek-cSDWAA&usqp=CAU',
+                    name: 'MEAN Stack For Web Developers: Build Websites on Javascript',
+                    progress: '25'
+                },
+                {
+                    img: 'https://desirecourse.net/wp-content/uploads/2018/09/7867856.jpg',
+                    name: 'The Coding Interview Bootcamp: Algorithms + Data Structures',
+                    progress: '80'
+                }],
             notification: [{}]
         },
         category: Category.findAll(),
@@ -91,7 +92,7 @@ router.get('/', (req, res) => {
  * search
  */
 router.get('/courses', (req, res) => {
-    const { category } = req.query;
+    const {category} = req.query;
     res.render('/courses', {
         user
     })
@@ -99,14 +100,14 @@ router.get('/courses', (req, res) => {
 
 router.get('/courses/:id', (req, res) => {
     const reqId = req.params.id;
-    try{
+    try {
         const course = Course.findById(reqId);
-        res.render('pages/course-detail',{
+        res.render('pages/course-detail', {
             css: ['course-detail'],
             course,
         })
-    }catch(error){
-        console.log(res.console.error())
+    } catch (error) {
+        console.log(error)
     }
 })
 
@@ -115,7 +116,7 @@ router.get('/courses/:id', (req, res) => {
  * return {}
  */
 router.get('/search', (req, res) => {
-    const { criteria } = req.params;
+    const {criteria} = req.params;
     res.send(['list of course']);
 })
 
